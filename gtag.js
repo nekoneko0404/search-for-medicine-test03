7606e383eb198eb0d7f6e37a2a626aff56fd7595
hir kiy <neko.neko.0404@gmail.com>
2026-01-09 09:11:46 +0900

    GAタグ、メタタグ変更・追加

--- gtag.js
+++ gtag.js
@@ -1,5 +1,5 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
-gtag('config', 'G-VXSZGE4HP7');
+gtag('config', 'G-H44R8N7W32');
